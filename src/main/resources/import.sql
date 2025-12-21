07de1d4639a7eef7cedcb0d5ddb27ecf679c7abc
PaulaBuscacio <paula@buscacio.net>
2025-12-21 17:20:04 -0300

    api com panache e h2

---
+++ src/main/resources/import.sql
@@ -0,0 +1,8 @@
+INSERT INTO pessoa (id, nome, anonascimento) VALUES (1,'Ana Silva', 1990);
+INSERT INTO pessoa (id, nome, anonascimento) VALUES (2,'Bruno Costa', 1985);
+INSERT INTO pessoa (id, nome, anonascimento) VALUES(3,'Carla Souza', 2000);
+INSERT INTO pessoa (id, nome, anonascimento) VALUES(4,'Daniel Oliveira', 1995);
+INSERT INTO pessoa (id, nome, anonascimento) VALUES(5,'Elisa Pereira', 2000);
+
+
+ALTER SEQUENCE pessoa_seq RESTART WITH 6;
